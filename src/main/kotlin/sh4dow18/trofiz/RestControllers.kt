86781ea874a438703833e7b0431a329b8328c44b
sh4dow18 <ramses.solano.arias@gmail.com>
2024-10-27 00:48:54 -0600

    add: find all platforms service functions
    The following is added:
    - "Find All Platforms" Service function
    - "Find All Platforms" Endpoint function

--- src/main/kotlin/sh4dow18/trofiz/RestControllers.kt
+++ src/main/kotlin/sh4dow18/trofiz/RestControllers.kt
@@ -1,6 +1,7 @@
 package sh4dow18.trofiz
 // Rest Controllers Requirements
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("\${endpoint.platforms}")
 class PlatformRestController(private val platformService: PlatformService) {
+    // When the Endpoint has HTTP GET requests, call find all platforms function
+    @GetMapping
+    @ResponseBody
+    fun findAll() = platformService.findAll()
     // When the Endpoint has HTTP POST requests, call insert platform function
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
--- src/main/kotlin/sh4dow18/trofiz/Services.kt
+++ src/main/kotlin/sh4dow18/trofiz/Services.kt
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional
 // Platform Service Interface where the functions to be used in
 // Spring Abstract Platform Service are declared
 interface PlatformService {
+    fun findAll(): List<PlatformResponse>
     fun insert(platformRequest: PlatformRequest): PlatformResponse
 }
 // Spring Abstract Platform Service
@@ -17,6 +18,10 @@ class AbstractPlatformService(
     @Autowired
     val platformMapper: PlatformMapper
 ): PlatformService {
+    override fun findAll(): List<PlatformResponse> {
+        // Returns all Platforms as a Platform Responses List
+        return platformMapper.platformsListToPlatformResponsesList(platformRepository.findAll())
+    }
     // @Transactional is a Tag that establishes that is a Transactional Service Function. This
     // one makes a transaction when this service function is in operation.
     @Transactional(rollbackFor = [ElementAlreadyExists::class])
