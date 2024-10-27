ae5beb98f15afdf4cb6b70aba9922ff9d5cabc70
sh4dow18 <ramses.solano.arias@gmail.com>
2024-10-27 01:55:10 -0600

    fix: insert platform test function
    Fix Platform Already Exists Validation in Insert Platform Test Function

--- src/test/kotlin/sh4dow18/trofiz/PlatformTests.kt
+++ src/test/kotlin/sh4dow18/trofiz/PlatformTests.kt
@@ -20,9 +20,12 @@ class PlatformTests(
     @Test
     fun insert() {
         // Insert Platform Test Prop
-        val platformRequest = PlatformRequest("Play Station 5")
+        val platformRequest = PlatformRequest("Plataforma de Prueba")
+        // Transforms Name in Platform Request in lowercase and replace spaces with "-"
+        // Example: "Play Station 5" -> "play-station-5"
+        val platformId = getPlatformId(platformRequest.name)
         // Verifies if the platform already exists
-        if (platformRepository.findByName(platformRequest.name).orElse(null) != null) {
+        if (platformRepository.findById(platformId).orElse(null) != null) {
             throw ElementAlreadyExists(platformRequest.name, "Plataforma")
         }
         // If not exists, create the new platform
